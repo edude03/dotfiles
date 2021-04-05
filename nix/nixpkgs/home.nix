917205d697c9d79836c5375f30e379977ee8d790
Michael Francis <edude03@gmail.com>
2021-04-05 19:59:12 -0400

    Cleanup

--- nix/nixpkgs/home.nix
+++ nix/nixpkgs/home.nix
@@ -1,4 +1,4 @@
-{ pkgs, ...}:
+{ pkgs, ... }:
 
 let
   zshConfig = (import ../../zsh/zshrc.nix).zshConfig;
@@ -43,10 +43,10 @@ let
       sha256 = "0yacjv7kv79yilsyij43m378shzln0qra5c3nc5g2mc2i9hxcial";
     };
   };
-in
-{
+in {
   programs.home-manager.enable = true;
-  programs.home-manager.path = https://github.com/rycee/home-manager/archive/master.tar.gz;
+  programs.home-manager.path =
+    "https://github.com/rycee/home-manager/archive/master.tar.gz";
 
   home.packages = with pkgs; [
     git
@@ -83,9 +83,7 @@ in
     dive
   ];
 
-  programs.fzf = {
-    enable = true;
-  };
+  programs.fzf = { enable = true; };
 
   programs.git = {
     enable = true;
@@ -93,110 +91,106 @@ in
     userEmail = "michael@melenion.com";
   };
 
- home.file.".tmux.conf" = {
-   text = tmuxConfig;
- };
+  home.file.".tmux.conf" = { text = tmuxConfig; };
 
- home.file.".hushlogin" = {
-   text = '''';
- };
+  home.file.".hushlogin" = { text = ""; };
 
- home.file.".bash_profile" = {
-   text = ''
-    exec zsh
-   '';
- };
+  home.file.".bash_profile" = {
+    text = ''
+      exec zsh
+    '';
+  };
 
- home.file.".gnupg/gpg-agent.conf" = {
-   text = ''
-    enable-ssh-support
-    default-cache-ttl 60
-    max-cache-ttl 120
-   '';
- };
+  home.file.".gnupg/gpg-agent.conf" = {
+    text = ''
+      enable-ssh-support
+      default-cache-ttl 60
+      max-cache-ttl 120
+    '';
+  };
 
   programs.zsh = {
-   enable = true;
-   initExtra = zshConfig;
+    enable = true;
+    initExtra = zshConfig;
   };
 
   programs.neovim = {
-		enable = true;
-		withPython  = false;  # I think I don't need it for now; [NOTE: rebuilds]
-		withPython3 = true;  #
-		vimAlias = true;
-			extraConfig = ''
-				" vim config
-				" Enable Line Numbers
-				set number
-				let g:airline_powerline_fonts = 1
-				" Show the name of the buffers in the tabline
-				let g:airline#extensions#tabline#enabled = 1
+    enable = true;
+    withPython = false; # I think I don't need it for now; [NOTE: rebuilds]
+    withPython3 = true;
+    vimAlias = true;
+    extraConfig = ''
+      				" vim config
+      				" Enable Line Numbers
+      				set number
+      				let g:airline_powerline_fonts = 1
+      				" Show the name of the buffers in the tabline
+      				let g:airline#extensions#tabline#enabled = 1
 
-				" Use spaces instead of tab char
-				set expandtab
+      				" Use spaces instead of tab char
+      				set expandtab
 
-				" Intend by 2 chars
-				set shiftwidth=2
-				set tabstop=2
+      				" Intend by 2 chars
+      				set shiftwidth=2
+      				set tabstop=2
 
-				" Turn on autoindent
-				set autoindent
+      				" Turn on autoindent
+      				set autoindent
 
-				" Set color scheme
-				set background=dark
+      				" Set color scheme
+      				set background=dark
 
-        " let g:airline_theme='quantum'
-				" let g:quantum_italics=1
-				set termguicolors
-				" colorscheme quantum
+              " let g:airline_theme='quantum'
+      				" let g:quantum_italics=1
+      				set termguicolors
+      				" colorscheme quantum
 
-				" Oceanic Next settings
-				syntax enable
-				colorscheme OceanicNext
-				let g:airline_theme='oceanicnext'
+      				" Oceanic Next settings
+      				syntax enable
+      				colorscheme OceanicNext
+      				let g:airline_theme='oceanicnext'
 
-				" Highlight the current line
-				set cursorline
+      				" Highlight the current line
+      				set cursorline
 
-				" Enable indent guide on startup
-				let g:indent_guides_enable_on_vim_startup = 1
+      				" Enable indent guide on startup
+      				let g:indent_guides_enable_on_vim_startup = 1
 
-				" Enables block folding
-				set foldmethod=syntax
+      				" Enables block folding
+      				set foldmethod=syntax
 
-        " Easy clearing of last search term
-        nnoremap <CR> :noh<CR><CR>
+              " Easy clearing of last search term
+              nnoremap <CR> :noh<CR><CR>
 
-        function! s:find_files()
-          let git_dir = system('git rev-parse --show-toplevel 2> /dev/null')[:-2]
-          if git_dir !=# '''
-            call fzf#vim#gitfiles(git_dir, fzf#vim#with_preview('right'))
-          else
-            call fzf#vim#files('.', fzf#vim#with_preview('right'))
-          endif
-        endfunction
-        command! ProjectFiles execute s:find_files()
-        nnoremap <silent> <C-p> :ProjectFiles<CR>
-			'';
+              function! s:find_files()
+                let git_dir = system('git rev-parse --show-toplevel 2> /dev/null')[:-2]
+                if git_dir !=# '''
+                  call fzf#vim#gitfiles(git_dir, fzf#vim#with_preview('right'))
+                else
+                  call fzf#vim#files('.', fzf#vim#with_preview('right'))
+                endif
+              endfunction
+              command! ProjectFiles execute s:find_files()
+              nnoremap <silent> <C-p> :ProjectFiles<CR>
+      			'';
 
-      plugins = with pkgs.vimPlugins // customPlugins; [
-        vim-airline
-        vim-indent-guides
-        nerdtree
-        fzf-vim
-        fugitive
-        ale
-        vim-better-whitespace
-        vim-gitgutter
+    plugins = with pkgs.vimPlugins // customPlugins; [
+      vim-airline
+      vim-indent-guides
+      nerdtree
+      fzf-vim
+      fugitive
+      ale
+      vim-better-whitespace
+      vim-gitgutter
 
-        # Syntax support
-        vim-nix
-        dhall-lang
+      # Syntax support
+      vim-nix
+      dhall-lang
 
-        # Themes
-        vim-quantum
-        vim-oceanic-next
-      ];
-	};
+      # Themes
+      vim-quantum
+      vim-oceanic-next
+    ];
+  };
 }
--- zsh/.zsh/plugins.sh
+++
@@ -1,4 +0,0 @@
-source /Users/edude03/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-bhilburn-SLASH-powerlevel9k/powerlevel9k.zsh-theme
-source /Users/edude03/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-caarlos0-SLASH-zsh-mkc/mkc.plugin.zsh
-source /Users/edude03/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
-source /Users/edude03/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-Tarrasch-SLASH-zsh-autoenv/autoenv.plugin.zsh
