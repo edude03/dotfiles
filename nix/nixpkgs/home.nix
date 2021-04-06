802befbfdbb1c3f42670346470f86f3999a89d92
Michael Francis <edude03@gmail.com>
2021-04-05 20:08:42 -0400

    Removed because this breaks gnome3 login on my linux systems

--- nix/nixpkgs/home.nix
+++ nix/nixpkgs/home.nix
@@ -95,12 +95,6 @@ in {
 
   home.file.".hushlogin" = { text = ""; };
 
-  home.file.".bash_profile" = {
-    text = ''
-      exec zsh
-    '';
-  };
-
   home.file.".gnupg/gpg-agent.conf" = {
     text = ''
       enable-ssh-support
@@ -119,60 +113,7 @@ in {
     withPython = false; # I think I don't need it for now; [NOTE: rebuilds]
     withPython3 = true;
     vimAlias = true;
-    extraConfig = ''
-      				" vim config
-      				" Enable Line Numbers
-      				set number
-      				let g:airline_powerline_fonts = 1
-      				" Show the name of the buffers in the tabline
-      				let g:airline#extensions#tabline#enabled = 1
-
-      				" Use spaces instead of tab char
-      				set expandtab
-
-      				" Intend by 2 chars
-      				set shiftwidth=2
-      				set tabstop=2
-
-      				" Turn on autoindent
-      				set autoindent
-
-      				" Set color scheme
-      				set background=dark
-
-              " let g:airline_theme='quantum'
-      				" let g:quantum_italics=1
-      				set termguicolors
-      				" colorscheme quantum
-
-      				" Oceanic Next settings
-      				syntax enable
-      				colorscheme OceanicNext
-      				let g:airline_theme='oceanicnext'
-
-      				" Highlight the current line
-      				set cursorline
-
-      				" Enable indent guide on startup
-      				let g:indent_guides_enable_on_vim_startup = 1
-
-      				" Enables block folding
-      				set foldmethod=syntax
-
-              " Easy clearing of last search term
-              nnoremap <CR> :noh<CR><CR>
-
-              function! s:find_files()
-                let git_dir = system('git rev-parse --show-toplevel 2> /dev/null')[:-2]
-                if git_dir !=# '''
-                  call fzf#vim#gitfiles(git_dir, fzf#vim#with_preview('right'))
-                else
-                  call fzf#vim#files('.', fzf#vim#with_preview('right'))
-                endif
-              endfunction
-              command! ProjectFiles execute s:find_files()
-              nnoremap <silent> <C-p> :ProjectFiles<CR>
-      			'';
+    extraConfig = builtins.readFile ../../vim/.vimrc;
 
     plugins = with pkgs.vimPlugins // customPlugins; [
       vim-airline
